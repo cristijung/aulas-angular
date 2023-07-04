f588e4e7771e6eaba655ccf57c5308877e1bb7d8
Cristina Jung <cristijung@gmail.com>
2023-07-04 15:31:45 -0300

    aula01 finalizada

--- Aula01/src/app/app-routing.module.ts
+++ Aula01/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RotasComponent } from './rotas/rotas.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'rotas', component: RotasComponent},
+  {path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
--- Aula01/src/app/app.module.ts
+++ Aula01/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RotasComponent } from './rotas/rotas.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RotasComponent
+    RotasComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
