46f96551bad5e200e0263e096cad8e335db7f7ab
Cristina Jung <cristijung@gmail.com>
2023-07-05 14:41:08 -0300

    aula02 em progresso

--- Aula02/src/app/app.module.ts
+++ Aula02/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
